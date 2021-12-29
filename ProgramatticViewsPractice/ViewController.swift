c2a60d0bd8e89d4950108cfd125066dd9e79f15e
kmatton@syr.edu <kmatton@syr.edu>
2021-12-28 21:50:33 -0800

    Initial Commit

---
+++ ProgramatticViewsPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProgramatticViewsPractice
+//
+//  Created by KanoaMatton on 12/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
